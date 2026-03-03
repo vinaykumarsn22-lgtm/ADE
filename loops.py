1638935f3595ab27550ccbd33665e26195033bee
vinaykumarsn22-lgtm <vinaykumarsn22@gmail.com>
2026-03-03 13:41:43 +0530

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,5 +1,5 @@
-rows = int(input("Enter the row size for the pattern: "))
-for i in range(1, rows + 1):
-    for j in range(1, i + 1): 
-        print("*") 
-    print()
+# rows = int(input("Enter the row size for the pattern: "))
+# for i in range(1, rows + 1):
+#     for j in range(1, i + 1): 
+#         print("*") 
+#     print()
