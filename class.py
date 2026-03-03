560883d71880d0245157f13977ddacf1c3b057c2
Vinay <vinaykumarsn22@gmail.com>
2026-03-03 14:19:16 +0530

    vs code push

--- class.py
+++ class.py
@@ -1,9 +1,16 @@
-class FirstClass:
-    class_name = 'This is my first class'
+# class FirstClass:
+#     class_name = 'This is my first class'
 
-    def test_function(self):
-        return 'This is a test function inside class'
+#     def test_function(self):
+#         return 'This is a test function inside class'
     
-my_first_class = FirstClass()
-print(my_first_class.class_name)
-print(my_first_class.test_function())
+# my_first_class = FirstClass()
+# print(my_first_class.class_name)
+# print(my_first_class.test_function())
+
+# constructor
+
+
+# git changed
+
+
